c966cd5c966a86855be1f50de7357a96b0e81bee
Arpit1069 <arpitvidhale08@gmail.com>
2023-06-08 15:48:44 +0530

    2nd commit

---
+++ Cmdarg.java
@@ -0,0 +1,11 @@
+package Code;
+
+import java.net.SocketPermission;
+
+public class Cmdarg {
+ public static void main(String [] args) {  //psvm
+    System.out.println(args [0]+ args [1]);
+    
+ }   
+  
+}
---
+++ Input.java
@@ -0,0 +1,9 @@
+package Code;
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.next());      
+    }
+}
